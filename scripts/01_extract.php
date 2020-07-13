48695b3e352077eb3d366f847909c5bbf3d02469
kiang <kiange@gmail.com>
2020-07-13 15:42:55 +0800

    first commit

---
+++ scripts/01_extract.php
@@ -0,0 +1,98 @@
+<?php
+$openGroups = array();
+$groupFh = array();
+foreach(glob(dirname(__DIR__) . '/openGroups/*.csv') AS $csvFile) {
+    $fh = fopen($csvFile, 'r');
+    fgetcsv($fh, 2048);
+    $fhCreated = false;
+    while($line = fgetcsv($fh, 2048)) {
+        if(false === $fhCreated) {
+            $fhCreated = true;
+            $rawPath = dirname(__DIR__) . '/raw/' . $line[12];
+            $groupFh[$line[12]] = array(
+                'expenditures' => $rawPath . '/expenditures.csv',
+                'incomes' => $rawPath . '/incomes.csv',
+            );
+        }
+        if(!isset($openGroups[$line[13]])) {
+            $openGroups[$line[13]] = array(
+                'group' => $line[12],
+                'expenditures' => 0,
+                'incomes' => 0,
+            );
+        }
+        if(!isset($openGroups[$line[14]])) {
+            $openGroups[$line[14]] = array(
+                'group' => $line[12],
+                'expenditures' => 0,
+                'incomes' => 0,
+            );
+        }
+    }
+}
+
+$zip = new ZipArchive;
+$expenditures = $incomes = array();
+foreach(glob('/home/kiang/public_html/ardata.cy.gov.tw/data/indifidual/account/109年立法委員選舉/*/*.zip') AS $zipFile) {
+    $fh = fopen("zip://{$zipFile}#expenditures.csv", 'r');
+    fgetcsv($fh, 2048);
+    while($line = fgetcsv($fh, 2048)) {
+        if(!isset($line[7])) {
+            continue;
+        }
+        $line[7] = trim($line[7]);
+        if(!empty($line[7]) && false === strpos($line[7], '*') && isset($openGroups[$line[7]])) {
+            $oFh = fopen($groupFh[$openGroups[$line[7]]['group']]['expenditures'], 'a+');
+            fputcsv($oFh, $line);
+            fclose($oFh);
+            $openGroups[$line[7]]['expenditures'] += intval($line[9]);
+        }
+    }
+
+    $fh = fopen("zip://{$zipFile}#incomes.csv", 'r');
+    fgetcsv($fh, 2048);
+    while($line = fgetcsv($fh, 2048)) {
+        if(!isset($line[7])) {
+            continue;
+        }
+        $line[7] = trim($line[7]);
+        if(!empty($line[7]) && false === strpos($line[7], '*') && isset($openGroups[$line[7]])) {
+            $oFh = fopen($groupFh[$openGroups[$line[7]]['group']]['incomes'], 'a+');
+            fputcsv($oFh, $line);
+            fclose($oFh);
+            $openGroups[$line[7]]['incomes'] += intval($line[8]);
+        }
+    }
+}
+foreach(glob('/home/kiang/public_html/ardata.cy.gov.tw/data/indifidual/account/109年總統、副總統選舉/*.zip') AS $zipFile) {
+    $fh = fopen("zip://{$zipFile}#expenditures.csv", 'r');
+    fgetcsv($fh, 2048);
+    while($line = fgetcsv($fh, 2048)) {
+        if(!isset($line[7])) {
+            continue;
+        }
+        $line[7] = trim($line[7]);
+        if(!empty($line[7]) && false === strpos($line[7], '*') && isset($openGroups[$line[7]])) {
+            $oFh = fopen($groupFh[$openGroups[$line[7]]['group']]['expenditures'], 'a+');
+            fputcsv($oFh, $line);
+            fclose($oFh);
+            $openGroups[$line[7]]['expenditures'] += intval($line[9]);
+        }
+    }
+
+    $fh = fopen("zip://{$zipFile}#incomes.csv", 'r');
+    fgetcsv($fh, 2048);
+    while($line = fgetcsv($fh, 2048)) {
+        if(!isset($line[7])) {
+            continue;
+        }
+        $line[7] = trim($line[7]);
+        if(!empty($line[7]) && false === strpos($line[7], '*') && isset($openGroups[$line[7]])) {
+            $oFh = fopen($groupFh[$openGroups[$line[7]]['group']]['incomes'], 'a+');
+            fputcsv($oFh, $line);
+            fclose($oFh);
+            $openGroups[$line[7]]['incomes'] += intval($line[8]);
+        }
+    }
+}
+file_put_contents(dirname(__DIR__) . '/report/01_extract.json', json_encode($openGroups));
